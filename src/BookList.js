ef9885a4c6b892f742f4a14f61c5637ae210df87
Monika <molinskamonika+github@gmail.com>
2018-07-18 08:44:48 +0200

    Filtering default books on shelves

--- src/BookList.js
+++ src/BookList.js
@@ -4,7 +4,8 @@ import BookShelf from './BookShelf'
 class BookList extends Component {
 
     render() {
-
+        const books = this.props.books
+        console.log(this.props.books)
         return (
             <div className="list-books">
                 <div className="list-books-title">
@@ -12,9 +13,9 @@ class BookList extends Component {
                 </div>
                 <div className="list-books-content">
                     <div>
-                        <BookShelf title="Currently Reading"/>
-                        <BookShelf title="Want to Read"/>
-                        <BookShelf title="Read"/>
+                        <BookShelf title="Currently Reading" books={books.filter((book) => (book.shelf === "currentlyReading"))} />
+                        <BookShelf title="Want to Read" books={books.filter((book) => (book.shelf === "wantToRead"))} />
+                        <BookShelf title="Read" books={books.filter((book) => (book.shelf === "read"))} />
                     </div>
                 </div>
 
